69b789e7e22bafca92bbb10368a7caddbd1a55e3
Benjamin Spriggs <bnplsprggs@gmail.com>
2018-03-08 12:39:41 -0600

    bleh

---
+++ ShooterPong/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject Ball;
+    public int direction;
+    public float yposition1 = 0.0f;
+    public float yposition2 = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+
+    void FireBall()
+    {
+        Instantiate(Ball, new Vector3(-12.5f, yposition1, 0.0f), Quaternion.identity);
+        new WaitForSeconds(1);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        yposition1 = GameObject.FindGameObjectWithTag("Paddle1").transform.position.y;
+        if (Input.GetKey("e"))
+         {
+            FireBall();
+         }
+    }
+}
